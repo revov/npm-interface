ad7eca49fb6e13bb3463977c01c8874782d40389
Stoyan Revov <st.revov@gmail.com>
2016-09-24 19:29:43 +0300

    Kill all running scripts upon exit

--- main/handlers/run-script.js
+++ main/handlers/run-script.js
@@ -1,8 +1,10 @@
-const {ipcMain} = require('electron');
+const {app, ipcMain} = require('electron');
 const ipcResponse = require('../ipc-response');
 const exec = require('child_process').exec;
 const esc = require('../escapeshellarg');
 const kill = require('tree-kill');
+
+let runningProcesses = {};
 
 ipcMain.on('run-script', function(event, eventId, packagePath, script) {
     const command = `npm explore ${esc(packagePath)} -- npm --color always run ${esc(script)}`;
@@ -19,12 +21,19 @@ ipcMain.on('run-script', function(event, eventId, packagePath, script) {
     });
 
     child.on('close', function(code) {
-        if(code == 0) {
-            ipcResponse.complete(event, eventId);
-        } else {
-            ipcResponse.error(event, eventId, code);
-        }
+        delete runningProcesses[child.pid];
+
+        try {
+            if(code == 0) {
+                ipcResponse.complete(event, eventId);
+            } else {
+                ipcResponse.error(event, eventId, code);
+            }
+        } catch(electronErr) {}
+        
     });
+
+    runningProcesses[child.pid] = true;
 
     // Handle script interrupts by user
     const stopScriptListener = function(innerEvent, innerEventId, innerPackagePath, innerScript) {
@@ -47,4 +56,33 @@ ipcMain.on('run-script', function(event, eventId, packagePath, script) {
     }
 
     ipcMain.on('stop-script', stopScriptListener);
+    
+});
+
+app.on('before-quit', (event) => {
+    if(Object.keys(runningProcesses).length == 0) {
+        return;
+    }
+
+    event.preventDefault();
+
+    let allKilled = [];
+
+    for(let pid in runningProcesses) {
+        allKilled.push(
+            new Promise((resolve, reject) => {
+                kill(pid, 'SIGKILL', killError => {
+                    if(killError) {
+                        console.log(`Error killing a subprocess ${pid}: ${killError}`);
+                    }
+                    resolve();
+                });
+            })
+        );
+    }
+
+    Promise.all(allKilled)
+        .then(
+            () => setImmediate(() => app.quit())
+        );
 });
