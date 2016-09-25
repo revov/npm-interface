bf28aaa92115db4152b475e2043c3ef1fd432969
Stoyan Revov <st.revov@gmail.com>
2016-09-25 19:46:52 +0300

    Fix escapeshellarg utility function

--- main/escapeshellarg.js
+++ main/escapeshellarg.js
@@ -1,9 +1,3 @@
 module.exports = function escapeshellarg (arg) {
-  var ret = ''
-
-  ret = arg.replace(/[^\\]'/g, function (m, i, s) {
-    return m.slice(0, 1) + '\\\''
-  })
-
-  return "'" + ret + "'"
+  return `'${arg.replace(/'/g, "'\\''")}'`;
 }
